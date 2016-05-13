1b15fe89045defc2d8688a477e4707665d2f3a27
lordstone <chengxis@usc.edu>
2016-05-13 09:01:50 +0800

    init 4th

---
+++ const.py
@@ -0,0 +1,1 @@
+URL_DOTAMAX_TOUR = 'http://www.dotamax.com/match/tour/'
--- crawler.py
+++ crawler.py
@@ -4,7 +4,23 @@ This is a sample crawler done by lordstone
 
 """
 
-
 import urllib
+import re
+
+import const
 
 
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+
+def test_getHtml():
+    html = getHtml(const.URL_DOTAMAX_TOUR)
+    print html
+
+
+print 'test html'
+test_getHtml()
