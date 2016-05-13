8f1493b9cc20d2acbdc67ffdecd9457871caa298
lordstone <chengxis@usc.edu>
2016-05-13 10:43:08 +0800

    finished init title proc

--- crawler.py
+++ crawler.py
@@ -1,16 +1,19 @@
+#-*-coding:utf8-*-
+
+
 """
 This is a sample crawler done by lordstone
 
 
 """
-
+import string
 import urllib
 import re
 import time
+from lxml import etree
 
 
 import const
-
 
 
 def getHtml(url):
@@ -19,9 +22,15 @@ def getHtml(url):
     return html
 
 
-def test_getHtml():
-    html = getHtml(const.URL_DOTAMAX_TOUR)
-    print html
+def get_from_etree(mytree, attrib):
+    """
+    This is the function to get content from etree
+    :param mytree:
+    :param attrib:
+    :return:
+    """
+    contents = mytree.xpath("//" + attrib)
+    return contents
 
 
 def proc_league(myurl):
@@ -30,8 +39,22 @@ def proc_league(myurl):
     :param myurl:
     :return:
     """
-    print 'processing:', const.URL_DOTAMAX_PREFIX + myurl
-    print 'league name:',
+    this_url = const.URL_DOTAMAX_PREFIX + myurl
+    print 'processing:', this_url
+    html = getHtml(this_url)
+    html = html.replace('\n', ' ')
+    html = html.replace('\t', ' ')
+    html_tree = etree.HTML(html.decode('utf-8'))
+    # Getting its title
+    mytitle_list = get_from_etree(html_tree, 'title')
+    if mytitle_list:
+        title_string = mytitle_list[0].text
+        title_string = title_string.replace('<title>', ' ')
+        title_string = title_string.replace('</title>', ' ')
+        title_string = title_string.replace(u'-  Dotamax - 做国内第一游戏数据门户', ' ')
+        title_string = title_string.replace('\n', ' ')
+        title_string = title_string.strip()
+        print 'league name:', title_string
 
 
 def get_entry(content='', myfunc=None):
@@ -53,13 +76,10 @@ def get_entry(content='', myfunc=None):
     print 'You have found', x, 'leagues pages in this directory'
 
 
-"""
-print 'test html'
-test_getHtml()
-"""
-
-
 def main():
+    """
+    Driver function
+    """
     html = getHtml(const.URL_DOTAMAX_TOUR)
     get_entry(html, proc_league)
 
