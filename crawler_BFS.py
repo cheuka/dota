bacf4ab655b3e659de3b2243972f5beee22b1276
lordstone <chengxis@usc.edu>
2016-05-15 17:12:15 +0800

    formulating the BFS mode (1st time)

--- crawler_BFS.py
+++ crawler_BFS.py
@@ -2,10 +2,21 @@
 import const
 from utility import *
 import os
+import re
 
 
-def get_entries(html, proc_league=None):
-    pass
+def get_entries(content):
+    reg = r'\?league_id=[0-9]{1,6}'
+    league_re = re.compile(reg)
+    league_list = re.findall(league_re, content)
+    league_set = set(league_list)
+    x = 0
+    fp = open('saved_data/leagues_list.txt', 'w+')
+    for league_id in league_set:
+        fp.write('http://www.dotamax.com/match/tour_matches/'+league_id+'\n')
+        x += 1
+    print 'You have found', x, 'leagues pages in this directory. Wrote to "saved_data/leagues_list.txt'
+    fp.close()
 
 
 def proc_league():
@@ -20,7 +31,7 @@ def gen_league_list():
     code, html = getHtml(const.URL_DOTAMAX_TOUR)
     status = code // 100
     if status == 2 or status == 3:
-        get_entries(html, proc_league)
+        get_entries(html)
     else:
         print 'bad url link, program terminating...'
 
@@ -29,10 +40,9 @@ def main():
     """
     Driver function
     """
-    if os.path.exists('saved_data/league_list.txt'):
-        proc_league_list()
-    else:
+    if not os.path.exists('saved_data/leagues_list.txt'):
         gen_league_list()
+    proc_league_list()
 
 
 # start from here as the main driver
