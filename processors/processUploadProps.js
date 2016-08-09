2f3bc0d313bcec6c5441d38354b691bf975d598e
Xu Ruiqing <lpeter82218065@gmail.com>
2016-08-09 14:15:14 +0800

    Update processUploadProps.js

--- processors/processUploadProps.js
+++ processors/processUploadProps.js
@@ -23,7 +23,7 @@ function processUploadProps(entries, meta)
                 container.player_info = dota.player_info;
         		container.game_winner = dota.game_winnner;	
 				// lordstone: added team id and name
-container.radiant_team_id = dota.radiant_team_id;
+				container.radiant_team_id = dota.radiant_team_id;
         		container.radiant_team_name = dota.radiant_team_tag;
 				container.dire_team_name = dota.dire_team_tag;
 				container.dire_team_id = dota.dire_team_id;
