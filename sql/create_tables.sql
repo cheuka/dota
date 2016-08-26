b787766479f8021b542a003860e87d52e974cdc6
Chengxi (Chen) Shi <chengxis@usc.edu>
2016-08-26 11:43:08 -0700

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -192,7 +192,7 @@ CREATE TABLE team_match(
 	match_id bigint REFERENCES matches(match_id) ON DELETE CASCADE,
 	PRIMARY KEY (dire_team_id, radiant_team_id, match_id),
 	-- is_radiant boolean not null,
-	is_winner boolean not null,
+	is_radiant_winner boolean not null, -- is radiant team the winner
 	version varchar(30),
 	end_time bigint
 );
