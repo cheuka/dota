1714ebf1795c6fe773a183ef86fbe8b149272700
lordstone <chengxis@usc.edu>
2016-09-07 23:36:05 +0800

    rename to storedem

--- svc/storedem.js
+++ svc/storedem.js
@@ -1,14 +1,18 @@
 /**
- * Form a queue of waiting list for zip and unzip
+ * Form a queue of waiting list for save and load dem file from
+ * server, with bzip2 as the tech to compress/decompress
  * and processors
 **/
 var config = require('../config');
 var express = require('express');
 var app = express();
 var bzip = require('../store/bzip');
-var port = config.PORT || config.BZIP_PORT;
+var port = config.PORT || config.STOREDEM_PORT;
 var queue = require('../store/queue');
 var db = require('../store/db');
 var redis = require('../store/redis');
 var sQueue = queue.getQueue('savedem');
 var lQueue = queue.getQueue('loaddem');
+
+
+
