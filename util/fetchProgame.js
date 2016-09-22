9e8b4645eff337af59c9bc5e9608beb3c3eb379f
xu ruiqing <lpeter218065@gmail.com>
2016-09-22 22:51:00 +0000

    avoid fetch redudant match and dem file

--- util/fetchProgame.js
+++ util/fetchProgame.js
@@ -32,69 +32,91 @@ module.exports = function(db, cb) {
                 return next();
  
             async.eachSeries(result, function(match, next2) {
-                
-                var job = generateJob("api_details",{
-                    match_id: match.match_id
-                });
+                //rxu, if we have download the match for another team, we do not
+                //need to download again
+                db.select('team_id').from('fetch_team_match').where({
+                    'is_fetch': true,
+                    'match_id': match.match_id
+                }).asCallback(function(err, is_fetch_match) {
+                    if (is_fetch_match) {
+                        var tm = {
+                            team_id: is_fetch_match.team_id,
+                            match_id: match.match_id,
+                            is_fetched: true,
+                        };
 
-                getData({
-                    url: job.url,
-                    delay: 1000
-                }, function(err, body) {
-                    if (err) {
-                        console.log(err);
-                        return next2();
-                    }
-                    if (body.result) {
-                        var match = body.result;
-
-                        match.parse_status = 0;
-                        insertMatch(db, redis, match, {
-                            type: "api",
-                            attempts: 1,
-                        }, waitParse);
-                    }
-                });
-                
-                function waitParse(err, job)
-                {
-                    if (err)  {
-                        console.error(err.stack || err);
-                        return next2();
+                        queries.upsert(db, 'fetch_team_match', tm, {
+                            team_id: is_fetch_match.team_id,
+                            match_id: match.match_id
+                        }, function(err) {
+                            console.log(err);
+                            return next2();
+                        });
                     }
 
-                    if (job) {
-                        var poll = setInterval(function() {
-                            pQueue.getJob(job.jobId).then(function(job) {
-                                job.getState().then(function(state) {
-                                    console.log("waiting for parse job %s, currently in %s", job.jobId, state);
-                                    if (state === "completed") {
-                                        clearInterval(poll);
+                    var job = generateJob("api_details",{
+                        match_id: match.match_id
+                    });
 
-                                        var tm = {
-                                            team_id: team_id,
-                                            match_id: match.match_id,
-                                            is_fetched: true,
-                                        };
+                    getData({
+                        url: job.url,
+                        delay: 1000
+                    }, function(err, body) {
+                        if (err) {
+                            console.log(err);
+                            return next2();
+                        }
+                        if (body.result) {
+                            var match = body.result;
 
-                                        queries.upsert(db, 'fetch_team_match', tm, {
-                                            team_id: team_id,
-                                            match_id: match.match_id
-                                        }, function(err) {
-                                            console.log(err);
+                            match.parse_status = 0;
+                            insertMatch(db, redis, match, {
+                                type: "api",
+                                attempts: 1,
+                            }, waitParse);
+                        }
+                    });
+                    
+                    function waitParse(err, job)
+                    {
+                        if (err)  {
+                            console.error(err.stack || err);
+                            return next2();
+                        }
+
+                        if (job) {
+                            var poll = setInterval(function() {
+                                pQueue.getJob(job.jobId).then(function(job) {
+                                    job.getState().then(function(state) {
+                                        console.log("waiting for parse job %s, currently in %s", job.jobId, state);
+                                        if (state === "completed") {
+                                            clearInterval(poll);
+
+                                            var tm = {
+                                                team_id: team_id,
+                                                match_id: match.match_id,
+                                                is_fetched: true,
+                                            };
+
+                                            queries.upsert(db, 'fetch_team_match', tm, {
+                                                team_id: team_id,
+                                                match_id: match.match_id
+                                            }, function(err) {
+                                                console.log(err);
+                                                return next2();
+                                            });
+                                        }
+                                        else if (state !== "active" && state !== "waiting") {
+                                            clearInterval(poll);
+                                            console.log('fetch dem failed or parse failed');
                                             return next2();
-                                        });
-                                    }
-                                    else if (state !== "active" && state !== "waiting") {
-                                        clearInterval(poll);
-                                        console.log('fetch dem failed or parse failed');
-                                        return next2();
-                                    }
+                                        }
+                                    });
                                 });
-                            });
-                        }, 2000);
+                            }, 2000);
+                        }
                     }
-                }
+                });
             }, function(err) {
                 return next(); 
             });
