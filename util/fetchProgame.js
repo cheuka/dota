910a99b6e69e3223cc8a7c730e245e0a962c3edc
Ruiqing <lpeter82218065@gmail.com>
2016-09-25 17:15:40 +0000

    workable version

--- util/fetchProgame.js
+++ util/fetchProgame.js
@@ -14,130 +14,119 @@ var pQueue = queue.getQueue('parse');
 
 module.exports = function(db, cb) {
 	console.log('fetch pro games by team');
-    //var common_teams = [111474];
-	//async.eachSeries(common_teams, function(team, cb)
-	//async.eachSeries(constants.common_teams, function(team, next)
-    
-	/*
-    async.forEachLimit(constants.common_teams, 1, function(team, next) {
-		var team_id = team.team_id;
-		//var team_id = team;
-    */
 
-        //db.raw('select match_id, is_fetched from fetch_team_match where team_id = ? and start_time > ? order by start_time desc', [team_id, 1470009600])
-        db.select('match_id', 'team_id').from('fetch_team_match').where({
-            //'team_id' : team_id,
-            'is_fetched': false
-        }).where('start_time', '>', 1470009600)
-        .orderBy('start_time', 'desc')
-        .asCallback(function(err, result) {
-            if (!result || err)
-                return cb();
- 
-            async.eachSeries(result, function(match, next2) {
+    db.select('match_id', 'team_id').from('fetch_team_match').where({
+        'is_fetched': false
+    }).where('start_time', '>', 1470009600)
+    .orderBy('start_time', 'desc')
+    .asCallback(function(err, result) {
+        if (!result || err)
+            return cb();
 
-                var is_team_interested = false;
-                for (var i = 0; i < constants.common_teams.length; ++i) {
-                    if (constants.common_teams[i].team_id == match.team_id) {
-                        is_team_interested = true;
-                        break;
-                    }
+        async.eachSeries(result, function(match_cur, next2) {
+
+            var is_team_interested = false;
+            var name;
+            for (var i = 0; i < constants.common_teams.length; ++i) {
+                if (constants.common_teams[i].team_id == match_cur.team_id) {
+                    is_team_interested = true;
+                    name = constants.common_teams[i].name;
+                    break;
+                }
+            }
+
+            if (!is_team_interested) {
+                return next2();
+            }
+            console.log('interested team ' + name);
+
+            console.log('match id ' + match_cur.match_id);
+            //rxu, if we have download the match for another team, we do not
+            //need to download it again
+            db.select('team_id').from('fetch_team_match').where({
+                'is_fetched': true,
+                'match_id': match_cur.match_id
+            }).asCallback(function(err, is_fetch_match) {
+                if (is_fetch_match && is_fetch_match.length > 0) {
+                    var tm = {
+                        team_id: match.team_id,
+                        match_id: match.match_id,
+                        is_fetched: true,
+                    };
+
+                    queries.upsert(db, 'fetch_team_match', tm, {
+                        team_id: match_cur.team_id,
+                        match_id: match_cur.match_id
+                    }, function(err) {
+                        console.log(err);
+                        return next2();
+                    });
                 }
 
-                if (!is_team_interested) {
-                    return next2();
-                }
+                var job = generateJob("api_details",{
+                    match_id: match_cur.match_id
+                });
 
-                //rxu, if we have download the match for another team, we do not
-                //need to download it again
-                db.select('team_id').from('fetch_team_match').where({
-                    'is_fetched': true,
-                    'match_id': match.match_id
-                }).asCallback(function(err, is_fetch_match) {
-                    
-                    if (is_fetch_match) {
-                        var tm = {
-                            team_id: match.team_id,
-                            match_id: match.match_id,
-                            is_fetched: true,
-                        };
-
-                        queries.upsert(db, 'fetch_team_match', tm, {
-                            team_id: match.team_id,
-                            match_id: match.match_id
-                        }, function(err) {
-                            console.log(err);
-                            return next2();
-                        });
+                getData({
+                    url: job.url,
+                    delay: 1000
+                }, function(err, body) {
+                    if (err) {
+                        console.log(err);
+                        return next2();
                     }
+                    if (body.result) {
+                        var match = body.result;
 
-                    var job = generateJob("api_details",{
-                        match_id: match.match_id
-                    });
+                        match.parse_status = 0;
+                        insertMatch(db, redis, match, {
+                            type: "api",
+                            attempts: 1,
+                        }, waitParse);
+                        
+                        function waitParse(err, job) {
+                            if (err)  {
+                                console.error(err.stack || err);
+                                return next2();
+                            }
 
-                    getData({
-                        url: job.url,
-                        delay: 1000
-                    }, function(err, body) {
-                        if (err) {
-                            console.log(err);
-                            //return next2();
-                        }
-                        if (body.result) {
-                            var match2 = body.result;
+                            if (job) {
+                                var poll = setInterval(function() {
+                                    pQueue.getJob(job.jobId).then(function(job) {
+                                        job.getState().then(function(state) {
+                                            console.log("waiting for parse job %s for %s of %s, currently in %s", job.jobId, match_cur.match_id, match_cur.team_id, state);
+                                            if (state === "completed") {
+                                                clearInterval(poll);
 
-                            match2.parse_status = 0;
-                            insertMatch(db, redis, match2, {
-                                type: "api",
-                                attempts: 1,
-                            }, waitParse);
-                        }
-                    });
+                                                var tm = {
+                                                    team_id: match_cur.team_id,
+                                                    match_id: match_cur.match_id,
+                                                    is_fetched: true,
+                                                };
 
-                    function waitParse(err, job)
-                    {
-                        if (err)  {
-                            console.error(err.stack || err);
-                            //return next2();
-                        }
-
-                        if (job) {
-                            var poll = setInterval(function() {
-                                pQueue.getJob(job.jobId).then(function(job) {
-                                    job.getState().then(function(state) {
-                                        console.log("waiting for parse job %s for %s of %s, currently in %s", job.jobId, match.match_id, match.team_id, state);
-                                        if (state === "completed") {
-                                            clearInterval(poll);
-
-                                            var tm = {
-                                                team_id: match.team_id,
-                                                match_id: match.match_id,
-                                                is_fetched: true,
-                                            };
-
-                                            queries.upsert(db, 'fetch_team_match', tm, {
-                                                team_id: match.team_id,
-                                                match_id: match.match_id
-                                            }, function(err) {
-                                                console.log(err);
+                                                queries.upsert(db, 'fetch_team_match', tm, {
+                                                    team_id: match_cur.team_id,
+                                                    match_id: match_cur.match_id
+                                                }, function(err) {
+                                                    console.log(err);
+                                                    return next2();
+                                                });
+                                            }
+                                            else if (state !== "active" && state !== "waiting") {
+                                                clearInterval(poll);
+                                                console.log('fetch dem failed or parse failed');
                                                 return next2();
-                                            });
-                                        }
-                                        else if (state !== "active" && state !== "waiting") {
-                                            clearInterval(poll);
-                                            console.log('fetch dem failed or parse failed');
-                                            return next2();
-                                        }
+                                            }
+                                        });
                                     });
-                                });
-                            }, 2000);
+                                }, 2000);
+                            }
                         }
                     }
-               });
-            }, cb);
-        });
-	/*} function(err) {
-        return cb(err);
-    }); */
+                }); 
+            });
+        }, cb);
+    });
+	
 }
 
